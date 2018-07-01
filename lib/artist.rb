71b8403896a1e160f07d183e65e1ee8615c11f83
Edsel Caprice <caprice@mail.usf.edu>
2018-07-01 20:51:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,13 +14,18 @@ end
 def add_song(song)
 @songs << song
 song.artist = self
+@@all << song
 end
 
 def add_song_by_name(name)
 song = Song.new(name)
 @songs << song
 song.artist = self
+@@all << name
 end
 
+def self.song_count
+@@all.size
+end
 
 end
