f1f301850075576002c7721a434773f722ebf0e0
Edsel Caprice <caprice@mail.usf.edu>
2018-07-01 20:31:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,19 @@
 class Artist
-attr_accessor :name
+attr_accessor :name, :songs
 @@all = []
+@@songs = []
 def initialize(name)
 @name = name
 @@all << self
+end
+
+def songs
+@@songs
+end
+
+def add_song(song)
+@songs = song
+@@songs << song
 end
 
 
