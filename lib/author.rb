528b2e2d53610d2f1662a3099fd427eb8a88d052
Edsel Caprice <caprice@mail.usf.edu>
2018-07-01 21:18:20 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author
+attr_accessor :name, :posts
+@@all = []
+
+def initialize(name)
+@name = name
+@posts = []
+end
+
+def posts
+@posts
+end
+
+def add_post(post)
+@post << post
+post.author = self
+@@all << post
+end
+
+def add_post_by_title(title)
+post = Song.new(title)
+@posts << post
+post.author = self
+@@all << post
+end
+
+def self.song_count
+@@all.size
+end
+
+end
